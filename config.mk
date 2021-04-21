1a1ec29539363cb76549fb6e5478eed00cdd3bd9
wucke13 <wucke13@gmail.com>
2021-04-21 21:34:59 +0200

    add nix flake

--- config.mk
+++ config.mk
@@ -4,6 +4,16 @@
 # Location of SKE
 SKE_PATH ?= ..
 
+# Directory in which the partition archives can be found
+PARTITIONS_ROOT ?= .
+
+# Partition images from which the partitions shall be build
+PARTITION_IMAGES ?= $(shell find '$(PARTITIONS_ROOT)' -maxdepth 3 -name 'lib*.a')
+
+# Partitions to be build
+PARTITION_NAMES = $(patsubst lib%.a,%,$(notdir $(PARTITION_IMAGES)))
+
+
 # You shouldn't need to modify the following
 TARGET_CCPATH =
 TARGET_CCPREFIX = 
@@ -19,7 +29,7 @@ HOST_ASFLAGS +=
 HOST_LDFLAGS +=
 
 TARGET_CC = ${TARGET_CCPATH}${TARGET_CCPREFIX}gcc 
-TARGET_LD= ${TARGET_CCPATH}${TARGET_CCPREFIX}ld
+TARGET_LD = ${TARGET_CCPATH}${TARGET_CCPREFIX}ld
 
 # DO NOT MODIFY. ABSOLUTE path to the LTE distro. 
 LTE_PATH=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))/../
